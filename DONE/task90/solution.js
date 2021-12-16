33123c7815c2d71d5d49c9e6b3eb341ff0ecc50f
Yurii Tern <ternxstyle@gmail.com>
2021-12-16 02:45:00 +0200

    Экспорт и импорт

---
+++ DONE/task90/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Import constants from the constants.js file and log them to the console using console.log
+ *
+ * Each console.log should log a separate constant to the screen
+ * */
+import { API_URL } from './constants.js';
+import { MAX_THREADS } from './constants.js';
+
+console.log(API_URL);
+console.log(MAX_THREADS);
